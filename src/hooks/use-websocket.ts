be5a6020806de549d0152a03337486389c3f6939
Alan Chang <alanaudi@aireco.tw>
2024-09-04 10:27:50 +0800

    chore: update

--- src/hooks/use-websocket.ts
+++ src/hooks/use-websocket.ts
@@ -6,18 +6,22 @@ export default function useWsbsocket(url: string): [boolean, string | null] {
 	const ws = useRef(null)
 
 	useEffect(() => {
-		const socket = new WebSocket(url)
+		try {
+			const socket = new WebSocket(url)
 
-		socket.onopen = () => {
-			console.log('WebSocket connected:', url)
-			setIsReady(true)
+			socket.onopen = () => {
+				console.log('WebSocket connected:', url)
+				setIsReady(true)
+			}
+			socket.onclose = () => setIsReady(false)
+			socket.onmessage = (event) => setMessage(event.data)
+
+			ws.current = socket as any
+		} catch (error) {}
+
+		return () => {
+			if (ws.current) (ws.current as WebSocket).close()
 		}
-		socket.onclose = () => setIsReady(false)
-		socket.onmessage = (event) => setMessage(event.data)
-
-		ws.current = socket as any
-
-		return () => socket.close()
 	}, [])
 
 	// bind is needed to make sure `send` references correct `this`
